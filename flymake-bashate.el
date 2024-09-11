74717e61ef5d0c0c1192973ab89ad1917ff2c6f4
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-09-11 17:22:04 -0400

    Update header

--- flymake-bashate.el
+++ flymake-bashate.el
@@ -23,8 +23,8 @@
 ;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; The flymake-bashate provides a Flymake backend for the bashate, a style
-;; checker for Bash shell scripts.
+;; The flymake-bashate package provides a Flymake backend for the bashate, a
+;; style checker for Bash shell scripts.
 ;;
 ;; (Bashate is a Bash script syntax checker, enforcing a set of style and syntax
 ;; rules to ensure that your scripts are consistent, clean, and easy to read.)
