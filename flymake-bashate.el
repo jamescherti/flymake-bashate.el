c599d3c15c6f174a54c1f3d0081311758e682089
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-04-16 12:24:36 -0400

    Release: 1.0.4

--- flymake-bashate.el
+++ flymake-bashate.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2024-2025 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
-;; Version: 1.0.3
+;; Version: 1.0.4
 ;; URL: https://github.com/jamescherti/flymake-bashate.el
 ;; Keywords: tools
 ;; Package-Requires: ((flymake-quickdef "1.0.0") (emacs "27.1"))
